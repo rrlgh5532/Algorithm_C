082982d5ba7b1deb245a792fe77e0f9af01c6071
One27 <rrlgh5532@me.com>
2022-05-14 22:07:27 +0900

    Initial Commit

---
+++ Algorithm_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Algorithm_C
+//
+//  Created by 이기호 on 2022/05/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
